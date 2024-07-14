1aa7bbd04ef40c65fa77d9add7c044b859f34502
Diyorbekoffice <diyorbekboymirzayev0@gmail.com>
2024-07-15 02:02:36 +0500

    new

---
+++ index.js
@@ -0,0 +1,160 @@
+// Osonroq masalalar
+
+// // 1-masala
+// let a = 25;
+// let x = +Math.floor(a / 2);
+// let y = a % 2;
+// console.log('a sonining butun qismi: ' + x);
+// console.log('a sonining qoldiq qismi: ' + y);
+
+
+// // 2-masla
+// let a= 5, b=8;
+// if (a > b) {
+//     console.log(a * b);
+// } else if (a < b) {
+//     console.log(a + b);
+// } else {
+//     console.log(a - b);
+// }
+
+
+// // 3-masala
+// let a = 5;
+// if (a === 0 || a === 1) {
+//     console.log(1);
+// } else {
+//     let natija = 1;
+//     for (let i = 2; i <= a; i++) {
+//         natija *= i;
+//     }
+//     console.log("natija: " + natija); 
+// }
+
+
+// // 4-masala
+// let a = 2, b = 3, c = 4;
+// let natija = a * a + b * b + c * c;
+// console.log('natija: ' + natija);
+
+
+// // 5-masala
+// let a = 2, b = 3;
+// let natija = Math.pow(a, b);
+// console.log('natija: ' + natija);
+
+// // 6-masala
+// let a = 27;
+// let natija = Math.round(Math.sqrt(a));
+// console.log('natija: ' + natija);
+
+// // 7-masala
+// let a = 5, b = 3;
+// for (let i = 0; i < 5; i++) {
+//     console.log(a * b);
+// }
+
+// // 8-masala
+// let a = 5;
+// console.log(a * 10 - 7);
+
+// // 9-masala
+// let a = 27;
+// console.log(Math.pow(a, 1 / 3));
+
+// // 10-masala 
+// let a = 2, b = 4, c = 8;
+// console.log(Math.cbrt(a * b * c));
+
+
+
+
+// butun sonlarga oid masalalar:
+
+// // 1-masala
+// let L = 265;
+// console.log(L / 100);
+
+// // 2-masala
+// let M = 2650;
+// console.log(M / 1000);
+
+// // 3-masala
+// let F = 2650;
+// console.log(F / 1024);
+
+// // 4-masala
+// let a = 17, b = 3;
+// console.log(Math.floor(a / 3));
+
+// // 5-masala
+// let a = 17, b = 3;
+// console.log(Math.floor(a % 3));
+
+// // 6-masala
+// let a = 57;
+// let x = Math.floor(a / 10);
+// let y = a % 10;
+// console.log('onliklar xonasi: ' + x);
+// console.log('birliklar xonasi: ' + y);
+
+// // 7-masala
+// let a = 57;
+// const x = +Math.floor(a / 10);
+// const y = a % 10;
+// const s = x + y;
+// console.log('raqamlari yigindisi: ' + s);
+
+
+// // 8-masala
+// let a = 57;
+// const x = +Math.floor(a / 10);
+// const y = a % 10;
+// const s = y * 10 + x;
+// console.log('natija: ' + s);
+
+// // 9-masala
+// let a = 578;
+// const x = +Math.floor(a / 100);
+// console.log('yuzlar xonasi: ' + x);
+
+// // 10-masala
+// let a = 578;
+// const x = a % 10;
+// const y = Math.floor((a % 100) / 10);
+// console.log('birlar xonasi: ' + x);
+// console.log('onlar xonasi: ' + y);
+
+// // 11-masala
+// let a = 578;
+// const x = +Math.floor(a / 100);
+// const y = a % 10;
+// const z = +Math.floor((a % 100) / 10)
+// const s = x + y + z;
+// console.log('raqamlari yigindisi: ' + s);
+
+// // 12-masala
+// let a = 123;
+// const x = +Math.floor(a / 100);
+// const y = a % 10;
+// const z = +Math.floor((a % 100) / 10);
+// const s = y * 100 + z * 10 + x;
+// console.log('natija: ' + s);
+
+
+// // // 13-masala
+// let a = 951;
+// const x = Math.floor(a / 100);
+// const z = Math.floor(a % 100);
+// console.log('natija: ' + z + x);
+
+
+
+
+
+
+
+
+
+
+
